11a0293ae5dceb38e68414ca843d24f11664f712
23122K <hazy.parader-0r@icloud.com>
2024-02-15 15:33:08 +0100

    Initial commit

---
+++ Recruitment-assignment/Presentation/Recruitment_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recruitment_assignmentApp.swift
+//  Recruitment-assignment
+//
+//  Created by Patryk Maciąg on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recruitment_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
